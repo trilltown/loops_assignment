20dc30823cdae86f211af5bb4470104d41effad6
trilltown <phil.awich@gmail.com>
2019-09-30 21:48:25 -0400

    Problem 2 part a & b

--- loops_lab.js
+++ loops_lab.js
@@ -10,3 +10,27 @@
 // for (num = 100; num>0; num-=1){
 //     console.log(num)
 // }
+
+// Problem 2
+//A
+
+let max = 10
+
+// while (max < 200) {
+//     if (max % 2 === 0) {
+//     console.log("This number is Even " + max)
+// } else {
+//     console.log("This number is odd " + max)
+// }
+// max++; 
+// }
+
+//B
+
+for (max = 0; max <= 20; max++) {
+    if(max % 2 === 0) {
+    console.log("The number is Even " + max)
+}   else {
+    console.log("This number is odd " + max)
+}
+}
